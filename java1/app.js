3d6ce28a782fa7669bf9bb89c8d4c2ff6c3a1c51
Iam Furkan <bejtovicfurkannn000@gmail.com>
2021-10-20 12:05:18 +0200

    java

---
+++ java1/app.js
@@ -0,0 +1,18 @@
+var1=5
+var2=6
+var3=7
+console.log(" O═a+b+c",var1+var2+var3);
+prvavariabla= 5+7;
+console.log(prvavariabla);
+console.log(prvavariabla);
+console.log(prvavariabla);
+console.log(prvavariabla);
+console.log(prvavariabla);
+console.log(prvavariabla);
+druga= "string";
+console.log(druga);
+console.log(druga);
+console.log(druga);
+console.log(druga);
+console.log(druga);
+console.log(druga);
---
+++ java2/app.js
@@ -0,0 +1,15 @@
+console.log("zadatak 1.")
+pravougaonik=15
+pravougaonik2=10
+
+"P=a*b"
+
+console.log("Resenje",15*10)
+
+console.log("zadatak 2.")
+pravougaonik=20
+pravougaonik2=35
+
+"P=a*b"
+
+console.log("Resenje",20*35)

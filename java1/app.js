10750fd4b74071a9677eb9e7b655767ba34680e3
Iam Furkan <bejtovicfurkannn000@gmail.com>
2021-11-18 19:59:54 +0100

    comit

--- java1/app.js
+++ java1/app.js
@@ -1,18 +1,69 @@
-var1=5
-var2=6
-var3=7
-console.log(" O═a+b+c",var1+var2+var3);
-prvavariabla= 5+7;
-console.log(prvavariabla);
-console.log(prvavariabla);
-console.log(prvavariabla);
-console.log(prvavariabla);
-console.log(prvavariabla);
-console.log(prvavariabla);
-druga= "string";
-console.log(druga);
-console.log(druga);
-console.log(druga);
-console.log(druga);
-console.log(druga);
-console.log(druga);
+_
+// var1="Hello world"
+
+// console.log(var1.length)
+
+// var2 = "Hello world"
+
+// console.log(var2[7])
+
+// var3="Hello world"
+
+// console.log(var3.slice(5, 11))
+
+// var4="Hello world"
+
+// var5=var4.slice(-5, -1)
+
+// console.log(var5)
+
+// text="Please visit Microsoft!"
+
+// newText=text.replace ("Microsoft","javascript")
+
+// console.log(text)
+// console.log(newText)
+
+// text="DobAr DaN"
+
+// var6=text.toLowerCase()
+
+// var7=text.toLowerCase()
+// console.log(var6)
+// console.log(var7)
+
+// text="Hello"
+
+// var8=text.concat(" ","bilo sta")
+// console.log(var8)
+
+// var1=["banana","Orange","apple"]
+// var2=var1.join("-")
+// console.log(var1)
+// console.log(var2)
+
+// var1=["banana","orange","apple","mango"]
+
+// console.log(var1)
+
+// vracenaVrednost=var1.pop()
+
+// console.log(var1)
+
+// console.log(vracenaVrednost)
+
+
+// var1=["banana","orange","apple","mango"]
+
+// console.log(var1)
+
+// vracenaVrednost=var1.unshift("Kiwi")
+
+// console.log(var1)
+
+// console.log(vracenaVrednost)
+
+
+
+
+
